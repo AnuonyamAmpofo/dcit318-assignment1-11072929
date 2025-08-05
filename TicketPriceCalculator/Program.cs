cfb50d359352dcf33908604ea1fa8f22b2f76697
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2025-08-05 13:22:35 +0000

    Add Ticket Price Calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            double ticketPrice;
+
+            if (age < 12 || age >= 65)
+                ticketPrice = 7.00;
+            
+            else
+                ticketPrice = 10.00;
+
+            Console.WriteLine($"The ticket price is: ${ticketPrice}");
+        }
+    }
+}
