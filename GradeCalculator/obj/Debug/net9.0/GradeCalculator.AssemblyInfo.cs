5889f1de553f40a146db5776a65b73fc343d5dfe
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2025-08-05 13:06:30 +0000

    Grade Calculator

--- GradeCalculator/obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
+++ GradeCalculator/obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243bff7427214ee401430bfff782e968e7ec20cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
